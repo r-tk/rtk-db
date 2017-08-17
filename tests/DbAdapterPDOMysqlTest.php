cd8a5d5504f43a4fd557bf3ce595b4818d0ab590
raul <raulraat@gmail.com>
2017-08-17 20:26:00 +0300

    random tests

--- tests/DbAdapterPDOMysqlTest.php
+++ tests/DbAdapterPDOMysqlTest.php
@@ -2,6 +2,7 @@
 // vendor/bin/phpunit --configuration phpunit_mysql.xml --coverage-text
 namespace DBAdapterPDOMysqlTest;
 
+use Exception;
 use PDO;
 use PHPUnit\Framework\TestCase;
 use RTK\DB\DB;
@@ -9,6 +10,8 @@ use RTK\DB\DB;
 class DBAdapterPDOMysqlTest extends TestCase {
 
 	const INSTANCE_NAME = 'DBAdapterPDOMysqlTest';
+
+	const TABLE_NAME = 'DBAdapterPDOMysqlTest';
 
 	public static function setUpBeforeClass() {
 
@@ -30,6 +33,35 @@ class DBAdapterPDOMysqlTest extends TestCase {
 		}
 
 		DB::addInstance(self::INSTANCE_NAME, $settings);
+
+	}
+
+	public function setUp() {
+
+		$db = DB::get(self::INSTANCE_NAME);
+
+		$res = $db->query('show tables like "' . self::TABLE_NAME . '"');
+		$res->fetchAll(PDO::FETCH_ASSOC);
+
+		if ($res->rowCount() !== 0) {
+			throw new Exception('table "' . self::TABLE_NAME . '" should not exist in test database');
+		}
+
+		$sql = 'CREATE TABLE ' . self::TABLE_NAME . '('
+			. 'id INT NOT NULL AUTO_INCREMENT,'
+			. 'foo VARCHAR(255),'
+			. 'bar VARCHAR(255),'
+			. 'PRIMARY KEY (id)'
+			. ')';
+
+		$db->exec($sql);
+
+	}
+
+	public function tearDown() {
+
+		$db = DB::get(self::INSTANCE_NAME);
+		$db->exec('DROP TABLE ' . self::TABLE_NAME);
 
 	}
 
@@ -75,6 +107,58 @@ class DBAdapterPDOMysqlTest extends TestCase {
 		$db = DB::get(self::INSTANCE_NAME);
 
 		$this->assertTrue($db->isConnected());
+
+	}
+
+	public function testLastInsertId() {
+
+		$db = DB::get(self::INSTANCE_NAME);
+
+		$res = $db->query("INSERT INTO " . self::TABLE_NAME . " set foo = 1, bar = 2;");
+		$this->assertEquals($db->lastInsertId(), 1);
+
+		$res = $db->query("INSERT INTO " . self::TABLE_NAME . " set foo = 1, bar = 2;");
+		$this->assertEquals($db->lastInsertId(), 2);
+
+	}
+
+	public function testFetch() {
+
+		$db = DB::get(self::INSTANCE_NAME);
+
+		$db->query("INSERT INTO " . self::TABLE_NAME . " set foo = 'something', bar = 'elsething'");
+
+		$res = $db->query('SELECT foo, bar FROM ' . self::TABLE_NAME);
+		while ($row = $res->fetch(PDO::FETCH_ASSOC)) {
+			$this->assertEquals(
+				$row,
+				array(
+					'foo' => 'something',
+					'bar' => 'elsething',
+				)
+			);
+		}
+
+	}
+
+	public function testFetchAll() {
+
+		$db = DB::get(self::INSTANCE_NAME);
+
+		$db->query("INSERT INTO " . self::TABLE_NAME . " set foo = 'something', bar = 'elsething'");
+
+		$res = $db->query('SELECT foo, bar FROM ' . self::TABLE_NAME);
+		$rows = $res->fetchAll(PDO::FETCH_ASSOC);
+
+		$this->assertEquals(
+			$rows,
+			array(
+				array(
+					'foo' => 'something',
+					'bar' => 'elsething',
+				)
+			)
+		);
 
 	}
 
